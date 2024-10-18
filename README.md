18d48f557e4780894dde2566306f8e7c0a7b0b2b
gasmelia <gasmelia@gmail.com>
2024-10-17 23:36:55 -0300

    first update files

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# terra-action-lambda-lab
+## terra-action-lambda-lab
 Deploy AWS Lambda function with Terraform via GitHub Actions CI/CD
+
+## Workflow
+VisualStudio Code - Terraform - GitHub Actions - AWS Lambda - Python code
+
+## Steps
+1 - Create AWS lambda function
+2 - Create .github/workflows folder and lambda_deplyment.yml file
+
+
+
+References:
+https://github.com/marketplace/actions/aws-lambda-deploy
---
+++ project/aws-lambda/aws-lambda.tf
@@ -0,0 +1,49 @@
+## IAM policy assume role for lambda function
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+## IAM role for lambda function
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+## zip the function to be run at function App.
+data "archive_file" "lambda" {
+    type        = "zip"
+    source_file = "../app/lambda_function.py"
+    output_path = "lambda_function.zip"
+}
+
+## test lambda function
+resource "aws_lambda_function" "test_lambda" {
+  filename      = "lambda_function.zip"
+  function_name = "lambda_function"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.8"
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}   
+
+
+
+
+
